52993b2d5261187d93d5374e9bdfe2dca6f63408
shway250 <jackson_duhon@yahoo.com>
2016-10-24 21:35:23 -0700

    deleted bonfire file, started palindromes function

--- palindromes.js
+++ palindromes.js
@@ -4,3 +4,18 @@
 // You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything lower case in order to check for palindromes.
 // We'll pass strings with varying formats, such as "racecar", "RaceCar", and "race CAR" among others.
 // We'll also pass strings with special symbols, such as "2A3*3a2", "2A3 3a2", and "2_A3*3#A2".
+
+
+function palindrome(str) {
+  for(var i = 0; i < str.length; i++){
+    for(var j = str.length; j > 0; j--){
+      
+    }
+  }
+
+  return true;
+}
+
+
+
+palindrome("eye");
