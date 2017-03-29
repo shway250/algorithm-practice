00f92aa36d7a1ad887be1e1ee89e5919de7cf4fa
shway250 <jackson_duhon@yahoo.com>
2017-03-29 16:46:02 -0700

    added factorial exercise

--- rubyExercises.rb
+++ rubyExercises.rb
@@ -73,3 +73,25 @@ def print_contacts(farge)
 end
 
 print_contacts(contacts);
+
+# Write a method to compute the `factorial` of a number.
+# Given a whole number n, a factorial is the product of all
+# whole numbers from 1 to n.
+# 5! = 5 * 4 * 3 * 2 * 1
+#
+# Example method call
+#
+# factorial(5)
+#
+# > 120
+#
+
+def factorial(num)
+  temp = 1;
+  for i in (1..num) do
+    temp = temp * i
+  end
+  puts temp;
+end
+
+factorial(5);
