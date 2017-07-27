c813b6c21d49130ec025c86e8d96115f35426f5d
shway250 <jackson_duhon@yahoo.com>
2017-07-26 18:18:44 -0700

    finished mutations algorithm

--- mutations.js
+++ mutations.js
@@ -7,19 +7,17 @@
 // Lastly, ["Alien", "line"], should return true because all of the letters in "line" are present in "Alien".
 
 function mutation(arr) {
-  var fargeALpha = whoWansaHighNah;
-  var firstStr = arr[0];
-  var secondStr = arr[1];
-  var trueArr = [];
-  for(var i = 0; i< firstStr.length; i++){
-   if(firstStr[i] === secondStr[i]){
-    trueArr.push("1");
-   }
+  arr[0] = arr[0].toLowerCase();
+  arr[1] = arr[1].toLowerCase();
+  for(var i = 0; i<arr[1].length; i++){
+    console.log(arr[1][i]);
+    if(arr[0].indexOf(arr[1][i]) === -1){
+      return false;
+    }else{
+      console.log('true for now');
+    }
   }
-  if(trueArr.length === firstStr.length){
-    return true;
-  }
-  return false;
+  return true;
 }
 
 mutation(["hello", "hey"]);
