7e9634eb96aee2ec0a36e966c7d320ba6289d658
shway250 <jackson_duhon@yahoo.com>
2017-12-13 16:40:56 -0800

    did some filter iterator method practice

---
+++ filterPractice.js
@@ -0,0 +1,28 @@
+//function that returns numbers bigger than 100
+
+function biggerThanC(arr){
+  return arr.filter((item)=> item>100);
+}
+
+var numArr = [1,2,345,654,23,12312,6];
+
+biggerThanC(numArr);
+
+//function that returns words that contain an W 
+function containsAW(arr){
+  return arr.filter( item => item.indexOf("w")>=0);
+}
+
+var strArr = ["straw", "what the fuck?", 'this has no dubs'];
+
+containsAW(strArr);
+
+//returns an array containing all the singular items from two arrays
+
+function findTheDiff(arr1, arr2){
+  return arr1.concat(arr2).filter(item => !arr1.includes(item) || !arr2.includes(item));
+}
+var first = [1,2,3,4,5,6,7];
+var second = [1,2,4,5,6,8,0];
+
+findTheDiff(first, second);
